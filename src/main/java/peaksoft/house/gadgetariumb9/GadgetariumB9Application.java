a382b5373de2e4dcfc72cb03313902809a46544f
Datka Mamatzhanova <datkascnu@gmail.com>
2023-07-03 16:48:29 +0600

    first commit

---
+++ src/main/java/peaksoft/house/gadgetariumb9/GadgetariumB9Application.java
@@ -0,0 +1,13 @@
+package peaksoft.house.gadgetariumb9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetariumB9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GadgetariumB9Application.class, args);
+    }
+
+}
---
+++ src/test/java/peaksoft/house/gadgetariumb9/GadgetariumB9ApplicationTests.java
@@ -0,0 +1,13 @@
+package peaksoft.house.gadgetariumb9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetariumB9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
