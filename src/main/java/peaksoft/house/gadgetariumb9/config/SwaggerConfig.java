0149ab21bc188ad1b8e2749a12a4b985689095d8
Salymbek <calumbekcaatbekov@gmail.com>
2023-07-13 12:27:50 +0600

    clean code

--- src/main/java/peaksoft/house/gadgetariumb9/config/SwaggerConfig.java
+++ src/main/java/peaksoft/house/gadgetariumb9/config/SwaggerConfig.java
@@ -8,23 +8,27 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.Collections;
+
 @Configuration
 public class SwaggerConfig {
-    private static final String API_KEY = "Bearer Token";
-    @Bean
-    public OpenAPI customOpenAPI(){
-        return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes(API_KEY, apiKeySecurityScheme()))
-                .info(new Info().title("Gadgetarium-b9"))
-                .security(Collections.singletonList(new SecurityRequirement().addList(API_KEY)));
-    }
-    public SecurityScheme apiKeySecurityScheme(){
-        return new SecurityScheme()
-                .name("Auth API")
-                .description("Please, put the token")
-                .in(SecurityScheme.In.HEADER)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("Bearer");
-    }
+
+  private static final String API_KEY = "Bearer Token";
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .components(new Components()
+            .addSecuritySchemes(API_KEY, apiKeySecurityScheme()))
+        .info(new Info().title("Gadgetarium-b9"))
+        .security(Collections.singletonList(new SecurityRequirement().addList(API_KEY)));
+  }
+
+  public SecurityScheme apiKeySecurityScheme() {
+    return new SecurityScheme()
+        .name("Auth API")
+        .description("Please, put the token")
+        .in(SecurityScheme.In.HEADER)
+        .type(SecurityScheme.Type.HTTP)
+        .scheme("Bearer");
+  }
 }
