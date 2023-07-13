68982cdf46cea0c9397101375d03e81852d01688
user <gulira.abdukerimkyzy.jk@gmail.com>
2023-07-13 17:58:40 +0600

    create s3 file

---
+++ src/main/java/peaksoft/house/gadgetariumb9/s3file/controller/S3FileController.java
@@ -0,0 +1,46 @@
+package peaksoft.house.gadgetariumb9.s3file.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import peaksoft.house.gadgetariumb9.s3file.service.S3FileService;
+
+@RestController
+@RequestMapping("/file")
+@RequiredArgsConstructor
+public class S3FileController {
+
+    private final S3FileService s3FileService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestPart(value = "file") MultipartFile file) {
+      return new ResponseEntity<>(s3FileService.uploadFile(file), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+      byte[] data = s3FileService.downloadFile(fileName);
+      ByteArrayResource resource = new ByteArrayResource(data);
+      return ResponseEntity
+          .ok()
+          .contentLength(data.length)
+          .header("Content-type", "application/octet-stream")
+          .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+          .body(resource);
+    }
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+      return new ResponseEntity<>(s3FileService.deleteFile(fileName), HttpStatus.OK);
+    }
+
+}
