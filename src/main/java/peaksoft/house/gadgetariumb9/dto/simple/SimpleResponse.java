a89b9b5d31bea8f0cae6734b8767e54f669f00b7
NITRO <sanadylov@gmail.com>
2023-07-17 11:08:39 +0600

    fix clean code

--- src/main/java/peaksoft/house/gadgetariumb9/dto/simple/SimpleResponse.java
+++ src/main/java/peaksoft/house/gadgetariumb9/dto/simple/SimpleResponse.java
@@ -1,5 +1,12 @@
 package peaksoft.house.gadgetariumb9.dto.simple;
 
-public class SimpleResponse {
+import lombok.Builder;
+import org.springframework.http.HttpStatus;
 
-}
+@Builder
+public record SimpleResponse (
+
+    String message,
+
+    HttpStatus status
+){}
