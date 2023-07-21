f29fdc657db80504d5fa92df289f916fa3deee6d
Datka Mamatzhanova <104366645+datka-mm@users.noreply.github.com>
2023-07-21 15:12:18 +0600

    Update MailingService.java

--- src/main/java/peaksoft/house/gadgetariumb9/service/MailingService.java
+++ src/main/java/peaksoft/house/gadgetariumb9/service/MailingService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 import peaksoft.house.gadgetariumb9.dto.request.authReqest.MailingRequest;
 import peaksoft.house.gadgetariumb9.dto.simple.SimpleResponse;
 
-@Service
 public interface MailingService {
   SimpleResponse sendHtmlEmail(MailingRequest mailingRequest);
 }
