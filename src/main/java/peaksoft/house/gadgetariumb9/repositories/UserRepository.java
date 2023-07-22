0078277063f3cf0ed82abe0b11cd4abc692f94cb
Davran2005 <davranjoldoshbaev2@gmail.com>
2023-07-22 14:17:46 +0600

    commit d method favorite

--- src/main/java/peaksoft/house/gadgetariumb9/repositories/UserRepository.java
+++ src/main/java/peaksoft/house/gadgetariumb9/repositories/UserRepository.java
@@ -1,9 +1,7 @@
 package peaksoft.house.gadgetariumb9.repositories;
 
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import peaksoft.house.gadgetariumb9.dto.response.subProductResponse.SubProductResponse;
 import peaksoft.house.gadgetariumb9.models.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
