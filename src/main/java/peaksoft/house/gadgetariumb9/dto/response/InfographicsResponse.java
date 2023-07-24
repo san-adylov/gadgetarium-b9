0a4c58133f1cad4ffc4fbbb801533d57ab87e51f
Salymbek <calumbekcaatbekov@gmail.com>
2023-07-24 14:21:23 +0600

    add infographics

--- src/main/java/peaksoft/house/gadgetariumb9/dto/response/InfographicsResponse.java
+++ src/main/java/peaksoft/house/gadgetariumb9/dto/response/InfographicsResponse.java
@@ -1,6 +1,33 @@
 package peaksoft.house.gadgetariumb9.dto.response;
 
+import java.math.BigDecimal;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
 public class InfographicsResponse {
 
+  private BigDecimal deliveredTotalPrice;
 
+  private int deliveredQuantity;
+
+  private BigDecimal waitingTotalPrice;
+
+  private int waitingQuantity;
+
+  private BigDecimal currentPeriod;
+
+  private BigDecimal previousPeriod;
+
+  public InfographicsResponse(BigDecimal deliveredTotalPrice, int deliveredQuantity,
+      BigDecimal waitingTotalPrice, int waitingQuantity, BigDecimal currentPeriod,
+      BigDecimal previousPeriod) {
+    this.deliveredTotalPrice = deliveredTotalPrice;
+    this.deliveredQuantity = deliveredQuantity;
+    this.waitingTotalPrice = waitingTotalPrice;
+    this.waitingQuantity = waitingQuantity;
+    this.currentPeriod = currentPeriod;
+    this.previousPeriod = previousPeriod;
+  }
 }
