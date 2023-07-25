6ffdf8fde45b08ec0fc6a4e434d0b8766a8eac05
as-aijamal <a.asangazieva@gmail.com>
2023-07-25 15:19:12 +0600

    add greetings page

--- src/main/java/peaksoft/house/gadgetariumb9/config/security/SecurityConfig.java
+++ src/main/java/peaksoft/house/gadgetariumb9/config/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
         .authorizeHttpRequests((authorizeHttpRequests) ->
             authorizeHttpRequests
                 .requestMatchers(
+                        "/",
                     "/api/v1/auth/**",
                     "/swagger-ui/**",
                     "v3/api-docs/**",
