5587d039d9abf94fd45bedb272db33f1fb3ba0c6
user <gulira.abdukerimkyzy.jk@gmail.com>
2023-08-02 12:27:49 +0600

    fixed

--- src/main/java/peaksoft/house/gadgetariumb9/apis/ProductApi.java
+++ src/main/java/peaksoft/house/gadgetariumb9/apis/ProductApi.java
@@ -6,8 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import peaksoft.house.gadgetariumb9.dto.request.product.ProductRequest;
+import peaksoft.house.gadgetariumb9.dto.response.subProduct.MainPagePaginationResponse;
 import peaksoft.house.gadgetariumb9.dto.simple.SimpleResponse;
 import peaksoft.house.gadgetariumb9.services.ProductService;
+import peaksoft.house.gadgetariumb9.services.SubProductService;
+
 import java.util.List;
 
 @RestController
@@ -18,6 +21,7 @@ import java.util.List;
 public class ProductApi {
 
     private final ProductService productService;
+    private final SubProductService subProductService;
 
     @PreAuthorize("hasAuthority('ADMIN')")
     @Operation(summary = "save Product", description = "Creating a new product")
@@ -31,5 +35,27 @@ public class ProductApi {
     @Operation(summary = "Get color", description = "Get the right colors")
     public List<String> getColors(@PathVariable String name) {
         return productService.getColor(name);
+    }
+
+    @GetMapping("/new")
+    @Operation(summary = "Get new products", description = "This method gets new products")
+    public MainPagePaginationResponse getNewProducts(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "5") int pageSize) {
+        return subProductService.getNewProducts(page,pageSize);
+    }
+
+    @GetMapping("/recommended")
+    @Operation(summary = "Get recommended products", description = "This method gets recommended products")
+    public MainPagePaginationResponse getRecommendedProducts(@RequestParam(defaultValue = "1") int page,
+                                                             @RequestParam(defaultValue = "5") int pageSize) {
+        return subProductService.getRecommendedProducts(page, pageSize);
+    }
+
+    @GetMapping("/discount")
+    @Operation(summary = "Get discount products", description = "This method gets all discount products")
+    public MainPagePaginationResponse getAllDiscountProducts(@RequestParam(defaultValue = "1") int page,
+                                                             @RequestParam(defaultValue = "5") int pageSize) {
+        return subProductService.getAllDiscountProducts(page, pageSize);
     }
 }
