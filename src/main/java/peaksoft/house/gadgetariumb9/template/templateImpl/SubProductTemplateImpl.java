796577621df82d982adc7b5b26bc1d7d2df6076d
NITRO <sanadylov@gmail.com>
2023-09-04 15:47:21 +0600

    Fixed main page methods

--- src/main/java/peaksoft/house/gadgetariumb9/template/templateImpl/SubProductTemplateImpl.java
+++ src/main/java/peaksoft/house/gadgetariumb9/template/templateImpl/SubProductTemplateImpl.java
@@ -98,75 +98,75 @@ public class SubProductTemplateImpl implements SubProductTemplate {
 
         params.add(subProductCatalogRequest.getGadgetType());
         if (subProductCatalogRequest.getBrandIds() != null) {
-            sql += "OR b.id = ANY (?)";
+            sql += "AND b.id = ANY (?)";
             params.add(subProductCatalogRequest.getBrandIds().toArray(new Long[0]));
         }
         if (subProductCatalogRequest.getPriceStart().compareTo(BigDecimal.ZERO) != 0) {
-            sql += " OR (s.price >= ?)";
+            sql += " AND (s.price >= ?)";
             params.add(subProductCatalogRequest.getPriceStart());
         }
         if (subProductCatalogRequest.getPriceEnd().compareTo(BigDecimal.ZERO) != 0) {
-            sql += " OR (s.price <= ?)";
+            sql += " AND (s.price <= ?)";
             params.add(subProductCatalogRequest.getPriceEnd());
         }
         if (!subProductCatalogRequest.getCodeColor().get(0).equalsIgnoreCase("string")) {
-            sql += " OR (s.code_color = ANY (?))";
+            sql += " AND (s.code_color = ANY (?))";
             params.add(subProductCatalogRequest.getCodeColor().toArray(new String[0]));
         }
         if (subProductCatalogRequest.getRam().get(0) > 0) {
-            sql += " OR (s.ram = ANY (?))";
+            sql += " AND (s.ram = ANY (?))";
             params.add(subProductCatalogRequest.getRam().toArray(new Integer[0]));
         }
         if (subProductCatalogRequest.getRom().get(0) > 0) {
-            sql += " OR (s.rom = ANY (?))";
+            sql += " AND (s.rom = ANY (?))";
             params.add(subProductCatalogRequest.getRom().toArray(new Integer[0]));
         }
         if (subProductCatalogRequest.getSim().get(0) > 0) {
-            sql += "OR (p.sim = ?)";
+            sql += " AND (p.sim = ?)";
             params.add(subProductCatalogRequest.getSim());
         }
         if (!subProductCatalogRequest.getBatteryCapacity().get(0).equalsIgnoreCase("string")) {
-            sql += "OR (p.battery_capacity = ANY (?))";
+            sql += " AND (p.battery_capacity = ANY (?))";
             params.add(subProductCatalogRequest.getBatteryCapacity().toArray(new String[0]));
         }
         if (subProductCatalogRequest.getScreenSize().get(0) > 0) {
-            sql += "OR (p.screen_size = ANY (?))";
+            sql += " AND (p.screen_size = ANY (?))";
             params.add(subProductCatalogRequest.getScreenSize().toArray(new Double[0]));
         }
         if (!subProductCatalogRequest.getScreenResolution().get(0).equalsIgnoreCase("string")) {
-            sql += "OR (s.screen_resolution = ANY (?))";
+            sql += " AND (s.screen_resolution = ANY (?))";
             params.add(subProductCatalogRequest.getScreenResolution().toArray(new String[0]));
         }
         if (subProductCatalogRequest.getProcessors().get(0).equalsIgnoreCase("string")) {
-            sql += " OR (l.processor = ANY (?))";
+            sql += " AND (l.processor = ANY (?))";
             params.add(subProductCatalogRequest.getProcessors().toArray(new String[0]));
         }
         if (subProductCatalogRequest.getPurposes().get(0).equalsIgnoreCase("string")) {
-            sql += " OR (l.purpose = ANY (?))";
+            sql += " AND (l.purpose = ANY (?))";
             params.add(subProductCatalogRequest.getPurposes().toArray(new String[0]));
         }
         if (subProductCatalogRequest.getVideoMemory() != null && subProductCatalogRequest.getVideoMemory().get(0) > 0) {
-            sql += "OR (l.video_memory = ANY(?))";
+            sql += " AND (l.video_memory = ANY(?))";
             params.add(subProductCatalogRequest.getVideoMemory().toArray(new Integer[0]));
         }
         if (subProductCatalogRequest.getHousingMaterials().get(0).equalsIgnoreCase("string")) {
-            sql += "OR (sw.housing_material = ANY (?))";
+            sql += " AND (sw.housing_material = ANY (?))";
             params.add(subProductCatalogRequest.getHousingMaterials().toArray(new String[0]));
         }
         if (subProductCatalogRequest.getMaterialBracelets().get(0).equalsIgnoreCase("string")) {
-            sql += "OR (sw.material_bracelet = ANY(?))";
+            sql += " AND (sw.material_bracelet = ANY(?))";
             params.add(subProductCatalogRequest.getMaterialBracelets().toArray(new String[0]));
         }
         if (subProductCatalogRequest.getGenders().get(0).equalsIgnoreCase("string")) {
-            sql += "OR (sw.gender = ANY(?))";
+            sql += " AND (sw.gender = ANY(?))";
             params.add(subProductCatalogRequest.getGenders().toArray(new String[0]));
         }
         if (subProductCatalogRequest.getInterfaces().get(0).equalsIgnoreCase("string")) {
-            sql += "OR (sw.an_interface = ANY(?))";
+            sql += " AND (sw.an_interface = ANY(?))";
             params.add(subProductCatalogRequest.getInterfaces().toArray(new String[0]));
         }
         if (subProductCatalogRequest.getHullShapes().get(0).equalsIgnoreCase("string")) {
-            sql += "OR (sw.hull_shape = ANY(?))";
+            sql += " AND (sw.hull_shape = ANY(?))";
             params.add(subProductCatalogRequest.getHullShapes().toArray(new String[0]));
         }
         if (subProductCatalogRequest.getSorting().equalsIgnoreCase("Новинки")) {
