528a4e7c64fafa67497eb6fab25db88845885b9a
Salymbek <calumbekcaatbekov@gmail.com>
2023-09-09 19:19:09 +0600

    fix get basket

--- src/main/java/peaksoft/house/gadgetariumb9/repositories/BasketRepository.java
+++ src/main/java/peaksoft/house/gadgetariumb9/repositories/BasketRepository.java
@@ -1,11 +1,13 @@
 package peaksoft.house.gadgetariumb9.repositories;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import peaksoft.house.gadgetariumb9.models.Basket;
 import java.util.List;
+import peaksoft.house.gadgetariumb9.models.User;
 
 @Repository
 public interface BasketRepository extends JpaRepository<Basket, Long> {
@@ -16,5 +18,7 @@ public interface BasketRepository extends JpaRepository<Basket, Long> {
 
     @Query("SELECT b FROM Basket b WHERE b.user.id = :userId")
     Basket findByUserId(@Param("userId") Long userId);
+
+    Optional<Basket> findByUser(User user);
 
 }
--- src/main/java/peaksoft/house/gadgetariumb9/services/serviceImpl/BasketServiceImpl.java
+++ src/main/java/peaksoft/house/gadgetariumb9/services/serviceImpl/BasketServiceImpl.java
@@ -1,6 +1,7 @@
 package peaksoft.house.gadgetariumb9.services.serviceImpl;
 
 import jakarta.transaction.Transactional;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -16,7 +17,6 @@ import peaksoft.house.gadgetariumb9.repositories.BasketRepository;
 import peaksoft.house.gadgetariumb9.repositories.SubProductRepository;
 import peaksoft.house.gadgetariumb9.services.BasketService;
 import peaksoft.house.gadgetariumb9.template.BasketTemplate;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,26 +34,52 @@ public class BasketServiceImpl implements BasketService {
 
     private final SubProductRepository subProductRepository;
 
-
     @Override
     public SimpleResponse saveBasket(Long subProductId) {
         User user = jwtService.getAuthenticationUser();
-        SubProduct subProduct = subProductRepository.findById(subProductId).orElseThrow(() -> {
-            log.error("Sub product with id: %s not found".formatted(subProductId));
-            return new NotFoundException("Sub product with id: %s not found".formatted(subProductId));
-        });
-        basketRepository.save(
-                Basket
-                        .builder()
-                        .user(user)
-                        .subProducts(List.of(subProduct))
-                        .build());
-        log.info("Basket with id: %s successfully saved".formatted(subProductId));
-        return SimpleResponse
-                .builder()
-                .message("Basket with successfully saved")
-                .status(HttpStatus.OK)
+
+        Optional<Basket> userBasket = basketRepository.findByUser(user);
+
+        if (userBasket.isPresent()) {
+            Basket basket = userBasket.get();
+            Optional<SubProduct> existingSubProduct = basket.getSubProducts()
+                .stream()
+                .filter(sp -> sp.getId().equals(subProductId))
+                .findFirst();
+
+            if (existingSubProduct.isPresent()) {
+                SubProduct subProduct = existingSubProduct.get();
+                subProduct.setQuantity(subProduct.getQuantity() + 1);
+                basket.getSubProducts().add(subProduct);
+                basketRepository.save(basket);
+            } else {
+                SubProduct subProduct = subProductRepository.findById(subProductId)
+                    .orElseThrow(() -> {
+                        log.error("Sub product with id: %s not found".formatted(subProductId));
+                        return new NotFoundException("Sub product with id: %s not found".formatted(subProductId));
+                    });
+                basket.getSubProducts().add(subProduct);
+            }
+        } else {
+            SubProduct subProduct = subProductRepository.findById(subProductId)
+                .orElseThrow(() -> {
+                    log.error("Sub product with id: %s not found".formatted(subProductId));
+                    return new NotFoundException("Sub product with id: %s not found".formatted(subProductId));
+                });
+
+            Basket newBasket = Basket.builder()
+                .user(user)
+                .subProducts(List.of(subProduct))
                 .build();
+
+            basketRepository.save(newBasket);
+        }
+
+        log.info("Basket with id: %s successfully saved".formatted(subProductId));
+        return SimpleResponse.builder()
+            .message("Basket successfully saved")
+            .status(HttpStatus.OK)
+            .build();
     }
 
 
