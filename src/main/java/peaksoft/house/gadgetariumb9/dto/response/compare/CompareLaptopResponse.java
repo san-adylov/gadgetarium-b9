ed104f7d2d9b4b9dfa68ea249c4f948ea5740286
NITRO <sanadylov@gmail.com>
2023-09-29 11:59:42 +0600

    delete constructor

--- src/main/java/peaksoft/house/gadgetariumb9/dto/response/compare/CompareLaptopResponse.java
+++ src/main/java/peaksoft/house/gadgetariumb9/dto/response/compare/CompareLaptopResponse.java
@@ -4,7 +4,6 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import peaksoft.house.gadgetariumb9.enums.Processor;
 import peaksoft.house.gadgetariumb9.enums.Purpose;
-import java.math.BigDecimal;
 
 @SuperBuilder
 @NoArgsConstructor
@@ -19,15 +18,4 @@ public class CompareLaptopResponse extends CompareProductResponse{
 
     private int video_memory;
 
-    public CompareLaptopResponse(Long subProductId, Long prId, String brandName, String prodName,
-                                 BigDecimal price, String color, String screen, int rom,
-                                 String operationalSystems, String catTitle, String subCatTitle,
-                                 String image, Processor processor, Purpose purpose,
-                                 double screen_size, int video_memory) {
-        super(subProductId, prId, brandName, prodName, price, color, screen, rom, operationalSystems, catTitle, subCatTitle, image);
-        this.processor = processor;
-        this.purpose = purpose;
-        this.screen_size = screen_size;
-        this.video_memory = video_memory;
-    }
 }
--- src/main/java/peaksoft/house/gadgetariumb9/dto/response/compare/CompareSmartPhoneResponse.java
+++ src/main/java/peaksoft/house/gadgetariumb9/dto/response/compare/CompareSmartPhoneResponse.java
@@ -18,15 +18,4 @@ public class CompareSmartPhoneResponse extends CompareProductResponse{
 
     private double screenSize;
 
-    public CompareSmartPhoneResponse(Long subProductId, Long prId, String brandName, String prodName,
-                                     BigDecimal price, String color, String screen, int rom,
-                                     String operationalSystems, String catTitle, String subCatTitle,
-                                     String image, int simCard, String diagonalScreen,
-                                     String batteryCapacity, double screenSize) {
-        super(subProductId, prId, brandName, prodName, price, color, screen, rom, operationalSystems, catTitle, subCatTitle, image);
-        this.simCard = simCard;
-        this.diagonalScreen = diagonalScreen;
-        this.batteryCapacity = batteryCapacity;
-        this.screenSize = screenSize;
-    }
 }
